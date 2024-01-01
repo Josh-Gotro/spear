dbffec5cd5a1248197e26466e8706ee541cf8b3c
Joshua Gauthreaux <gotro@Joshuas-Mac-mini.local>
2023-12-31 20:40:14 -0900

    improve odds

--- server/api/helpers.js
+++ server/api/helpers.js
@@ -1,4 +1,7 @@
 import pool from '../db.js';
+import fs from 'fs';
+import csv from 'csv-parser';
+import createCsvWriter from 'csv-writer';
 
 export async function checkIfNumberExists(number) {
   try {
@@ -6,7 +9,6 @@ export async function checkIfNumberExists(number) {
       'SELECT COUNT(*) FROM unique_numbers WHERE number = $1',
       [number]
     );
-    5;
     return queryResult.rows[0].count > 0;
   } catch (error) {
     console.error(error);
@@ -15,18 +17,51 @@ export async function checkIfNumberExists(number) {
 }
 
 export async function generateUniqueNumbers() {
-  let uniqueNumber;
   let uniqueNumbers = [];
   let numberExists;
 
-  while (uniqueNumbers.length < 5) {
-    do {
-      uniqueNumber = Math.floor(Math.random() * 10000);
-      numberExists = await checkIfNumberExists(uniqueNumber);
-    } while (numberExists);
+  // Read numbers from CSV file
+  const numbersFromCSV = [];
+  fs.createReadStream('./smn_comma.csv')
+    .pipe(csv())
+    .on('data', (row) => {
+      numbersFromCSV.push(Number(row.number));
+    })
+    .on('end', async () => {
+      console.log('CSV file successfully processed');
 
-    uniqueNumbers.push(uniqueNumber);
-  }
+      // Array to hold numbers that are not in the database
+      let numbersNotInDb = [];
 
-  return uniqueNumbers;
+      for (let i = 0; i < numbersFromCSV.length; i++) {
+        numberExists = await checkIfNumberExists(numbersFromCSV[i]);
+        if (!numberExists) {
+          uniqueNumbers.push(numbersFromCSV[i]);
+          numbersNotInDb.push(numbersFromCSV[i]);
+        }
+      }
+
+      // If there aren't enough unique numbers, generate more
+      while (uniqueNumbers.length < 5) {
+        let newNumber = Math.floor(Math.random() * 1000000);
+        numberExists = await checkIfNumberExists(newNumber);
+        if (!numberExists) {
+          uniqueNumbers.push(newNumber);
+        }
+      }
+
+      // Write the numbers that are not in the database back to the CSV file
+      const csvWriter = createCsvWriter({
+        path: './numbers.csv',
+        header: [{ id: 'number', title: 'NUMBER' }],
+      });
+
+      const records = numbersNotInDb.map((number) => ({ number }));
+
+      csvWriter
+        .writeRecords(records) // returns a promise
+        .then(() => {
+          console.log('...Done');
+        });
+    });
 }
